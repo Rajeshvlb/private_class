0c7e25cf3a019c8eb33af16fa5133d653d01c2b6
Rajesh K <42039541+Rajeshvlb@users.noreply.github.com>
2021-04-08 07:04:54 +0530

    Update threading.py

--- threading.py
+++ threading.py
@@ -1,3 +1,4 @@
+# without threading
 import time
 start = time.perf_counter()
 
@@ -7,5 +8,22 @@ def do_something():
   print('Done sleeping')
   
 do_something()
+finish = time.perf_counter()
+print(f'Finished in {round(finish-start, 2)} second(s)')
+
+
+#with threading
+import threading
+import time
+start = time.perf_counter()
+
+def do_something():
+  print('sleeping for 1 second...')
+  time.sleep(1)
+  print('Done sleeping')
+  
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something
+                      
 finish = time.perf_counter()
 print(f'Finished in {round(finish-start, 2)} second(s)')
