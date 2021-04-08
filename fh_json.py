965d5e3151112864db761f9ca96df910e09c6fa1
Rajesh K <42039541+Rajeshvlb@users.noreply.github.com>
2021-04-08 06:37:10 +0530

    Update fh_json.py

--- fh_json.py
+++ fh_json.py
@@ -1,1 +1,13 @@
+import json
+handle = open("json_input.json", "r")
+content = handle.read()
 
+d = json.loads(content) #json to dictionary
+
+j = json.dumps(d) #dictionary to json
+
+j = json.dumps(d, ident=4)
+handle.close()
+handle = open("json_output.json", "w")
+handle.write(j)
+handle.close()
